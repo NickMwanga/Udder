de36b58aac4a4109acec7bfd672e7eb0fd3cf08f
Nickmwanga <nickmwanga@gmail.com>
2024-11-16 14:23:28 +0300

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Nickson Mwanga on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
